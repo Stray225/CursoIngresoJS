04b9dcefe9e6734c18eec61ea71ccbc2fe1825bf
Agustín <40206693+Stray225@users.noreply.github.com>
2022-07-12 08:31:04 -0300

    7 if
    ni idea

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,10 +1,9 @@
 function mostrar()
 {
 	let edad = parseInt(txtIdEdad.value);
-	let estadoCivill = estadoCivil.value;
-	console.log(estadoCivill);
 
-	if (edad <= 17 && estadoCivill == Soltero);{
-		alert("Es muy pequeño para NO ser soltero");
+		if(estadoCivil.value == Soltero){
+			alert(edad);
+		}
 	}
-}
+
